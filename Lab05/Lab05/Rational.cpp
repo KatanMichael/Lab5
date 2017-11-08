6f7bee09e3b68bb84edb9817fef0bf760a043e1f
katanmichael <michaelkatan@gmail.com>
2017-11-08 20:03:37 +0200

    ..

--- Lab05/Lab05/Rational.cpp
+++ Lab05/Lab05/Rational.cpp
@@ -1,3 +1,60 @@
-#include "Rational.h"
+#include "stdafx.h"
 
-Rational::~Rational() {}
+#include "Rational.h"
+#include <iostream>
+
+using namespace std;
+
+void Rational::minNum()
+{
+	int temp;
+	temp = _n / _m;
+
+	
+}
+
+// (M/N) 
+Rational::Rational()
+{
+	_m = 1;
+	_n = 1;
+}
+
+
+// (M/N) 
+Rational::Rational(int m, int n)
+{
+	if (n == 0)
+	{
+		cout << "n cannot be 0... n is 1" << endl;
+		_n = 1;
+	}
+
+	_m = m;
+}
+
+int Rational::getM()
+{
+	return _m;
+}
+
+int Rational::getN()
+{
+	return _n;
+}
+
+Rational * Rational::setM(int m)
+{
+	_m = m;
+	return this;
+}
+
+Rational * Rational::setN(int n)
+{
+	if (n == 0)
+	{
+		cout << "n cannot be 0.. keeping old value" << endl;
+	}
+	return this;
+}
+
--- Lab05/Lab05/Rational.h
+++ Lab05/Lab05/Rational.h
@@ -5,14 +5,18 @@ private:
 	int _m;
 	int _n;
 
+	void minNum();
 
-
-
-public:
+	public:
 
 	Rational();
 	Rational(int, int);
 	~Rational();
 
+	int getM();
+	int getN();
+
+	Rational* setM(int m);
+	Rational* setN(int n);
 
 };
